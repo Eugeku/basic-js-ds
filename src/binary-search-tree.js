f6e5d7e277a60eb172b4357afe9cdc37839f5f04
Eugeku <eugenekuzora@gmail.com>
2025-01-05 17:59:06 +0300

    feat: implement max min remove functions

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -44,29 +44,111 @@ class BinarySearchTree {
     }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    const node = this.rootNode;
+    if (node === null) {
+      return false;
+    } else {
+      const findNode = function (node) {
+        if (data < node.data) {
+          if (node.left === null) {
+            return false;
+          } else if (node.left !== null) {
+            return findNode(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            return false;
+          } else if (node.right !== null) {
+            return findNode(node.right);
+          }
+        } else {
+          return data === node.data;
+        }
+      };
+      return findNode(node);
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    const node = this.rootNode;
+    if (node === null) {
+      return null;
+    } else {
+      const findNode = function (node) {
+        if (data < node.data) {
+          if (node.left === null) {
+            return null;
+          } else if (node.left !== null) {
+            return findNode(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            return null;
+          } else if (node.right !== null) {
+            return findNode(node.right);
+          }
+        } else if (data === node.data) {
+          return node;
+        } else {
+          return null;
+        }
+      };
+      return findNode(node);
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    const removeNode = (node, data) => {
+      if (node === null) {
+        return null;
+      } if (data === node.data) {
+        if (node.left === null && node.right === null) {
+          return null;
+        }
+        if (node.left === null) {
+          return node.right;
+        }
+        if (node.right === null) {
+          return node.left;
+        }
+        let tempNode = this.getMin(node.right);
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this.rootNode = removeNode(this.rootNode, data);
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let node = this.rootNode;
+    return this.getMin(node).data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let node = this.rootNode;
+    return this.getMax(node).data;
+  }
+
+  getMin(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  getMax(node) {
+    while (node.right !== null) {
+      node = node.right;
+    }
+    return node;
   }
 }
 
